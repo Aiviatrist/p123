84bb9a78e1422d8825b627d49a455cf31f24b506
Aiviatrist <92304985+Aiviatrist@users.noreply.github.com>
2022-04-12 19:24:21 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-lol
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,500);
+    canvas.position(560,150);
+    posenet=ml5.poseNet(video,loadmodel);
+    posenet.on("pose",gotPoses);
+}
+function loadmodel(){
+    console.log("model is loaded");
+}
+function gotPoses(results){
+      if(results.length>0){
+          console.log(results);
+      }
+}
+function draw(){
+    background("white");
+}
